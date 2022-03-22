fe46fc8c30459e29e202caeefc6e1a3b4796f959
Jesús Sánchez <jesussaco1@hotmail.com>
2022-03-21 22:29:25 -0600

    Jesus init

--- globales-fe/Tabs/Tabs.js
+++ globales-fe/Tabs/Tabs.js
@@ -2,6 +2,8 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Papaya from "../components/Papaya";
 import Melon from "../components/Melon";
+import Inbox from "../components/Inbox";
+
 const Tab = createBottomTabNavigator();
 const Tabs = () => (
   <Tab.Navigator
@@ -27,6 +29,16 @@ const Tabs = () => (
         tabBarLabel: "Updates",
         tabBarIcon: ({ color, size }) => (
           <MaterialCommunityIcons name="bell" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Inbox"
+      component={Inbox}
+      options={{
+        tabBarLabel: "Mensajeria",
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="android-messages" color={color} size={size} />
         ),
       }}
     />
---
+++ globales-fe/components/Inbox.js
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+const Inbox = () => (
+  <View>
+    <Text>Mensajeria...</Text>
+  </View>
+);
+
+export default Inbox;

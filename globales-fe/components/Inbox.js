10a5bf348db1e5c98d43027a1cdbd91018ebac2c
Jesús Sánchez <jesussaco1@hotmail.com>
2022-03-22 23:28:03 -0600

    Probando items para chat

--- globales-fe/components/Inbox.js
+++ globales-fe/components/Inbox.js
@@ -1,8 +1,41 @@
-import { View, Text } from "react-native";
-const Inbox = () => (
-  <View>
-    <Text>Mensajeria...</Text>
-  </View>
-);
+import { View, Text, FlatList } from "react-native";
+
+const Messages = [
+  {id:1,
+  userName:"Jesus Sanchez",
+  userImg:"img",
+  messageTime: "4 Minutes ago",
+  messageText:  "Hola soy Jesus",
+},
+{id:2,
+  userName:"Jesus Sanchez",
+  userImg:"img",
+  messageTime: "4 Minutes ago",
+  messageText:  "Hola soy Jesus",
+},
+{id:3,
+  userName:"Jesus Sanchez",
+  userImg:"img",
+  messageTime: "4 Minutes ago",
+  messageText:  "Hola soy Jesus",
+},
+]
+const Inbox = () => {
+
+
+  return(
+      <View>
+      <FlatList
+        data={Messages}
+        keyExtractor={item=>item.id}
+        renderItem={({item})=>(
+          <View>
+            <Text>{item.userName}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
 
 export default Inbox;

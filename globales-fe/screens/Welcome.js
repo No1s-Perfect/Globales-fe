02065487a18bda5aa006e4921ed6dedcda20da72
kevingg318 <56329768+kevingg318@users.noreply.github.com>
2022-03-26 14:37:38 -0600

    Update Welcome.js

--- globales-fe/screens/Welcome.js
+++ globales-fe/screens/Welcome.js
@@ -15,8 +15,8 @@ import{
 } from './../components/styles';
  
 const Welcome = ({navigation, route}) => {
-    const {name, email, photoUrl} = route.params;
-    const AvatarImg = photoUrl ? {uri:photoUrl} : require('./../assets/img/rayo.png')
+    //const {name, email, photoUrl} = route.params;
+    //const AvatarImg = photoUrl ? {uri:photoUrl} : require('./../assets/img/rayo.png')
 
     return (
         <>
@@ -26,10 +26,10 @@ const Welcome = ({navigation, route}) => {
             <WelcomeImage resizeMode="cover" source={require('./../assets/img/img1.png')}/>
                 <WelcomeContainer>
                 <PageTitle welcome={true}> Welcome </PageTitle>
-                <SubTile welcome={true}>{name || 'Name'}</SubTile>
-                <SubTile welcome={true}>{email || 'email'}</SubTile>
+                <SubTile welcome={true}>{/*name || 'Name'*/}name</SubTile>
+                <SubTile welcome={true}>{/*email || 'email'*/}email</SubTile>
                     <StyledFormArea>  
-                    <Avatar resizeMode="cover" source={AvatarImg || require('./../assets/img/img1.png')}/>
+                    <Avatar resizeMode="cover" source={require('./../assets/img/img1.png')}/>
                         <Line/>
                     <StyledButton onPress={()=>{navigation.navigate('Login');}}>
                         <ButtonText>Logout</ButtonText>

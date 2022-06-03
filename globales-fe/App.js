65024cb3de8bd922c797d068433df1fdd3dd0758
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-02 21:34:24 -0600

    Add files via upload

--- globales-fe/App.js
+++ globales-fe/App.js
@@ -1,18 +1,21 @@
-import React from "react";
-import RootStack from "./navigators/RootStack";
-import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./Tabs/Tabs";
+import React from 'react';
+import RootStack from './navigators/RootStack';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './Tabs/Tabs';
+import UserContext from './components/context/UserContext';
+import { useState } from 'react';
 export default function App() {
+  const [user, setUser] = useState(null);
   return (
     <>
-    {false && <RootStack/>}
-      <NavigationContainer>
-        <Tabs />
-      </NavigationContainer>
+      <UserContext.Provider value={{ user, setUser }}>
+        {!user && <RootStack />}
+        {user && (
+          <NavigationContainer>
+            <Tabs />
+          </NavigationContainer>
+        )}
+      </UserContext.Provider>
     </>
   );
-
 }
-
-
-

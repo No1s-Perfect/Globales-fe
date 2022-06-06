faefc3144323451ffbd07a1430b7b7191e4a06e5
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-05 19:57:50 -0600

    Add files via upload

--- globales-fe/components/Review.js
+++ globales-fe/components/Review.js
@@ -1,25 +1,56 @@
-import { AirbnbRating } from "react-native-ratings";
-import { TextInput, StyleSheet, View } from "react-native";
+import { AirbnbRating } from 'react-native-ratings';
+import { TextInput, StyleSheet, Pressable, Text, View } from 'react-native';
+import { useState } from 'react';
+import axios from 'axios';
+import Toast from "react-native-toast-message";
+const Review = ({setShow}) => {
+  const [rating, setRating] = useState(0);
 
-const Review = () => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = async() => {
+
+    try{
+      
+      await axios.post(process.env.REACT_APP_REVIEW,{
+        calificacion:rating,
+        retroalimentacion:text,
+        idUsuario:1,
+        idOferta:1
+      })
+      Toast.show({
+        type: "success",
+        text2: "Everything went smoothly 👋",
+      });
+      setShow(false)
+    }catch(e){
+      console.log(e)
+    }
+    
+  }
   return (
     <>
       <AirbnbRating
-        reviews={["Care", "Barro", "La", "Cago", "Puto"]}
+        reviews={['Care', 'Barro', 'La', 'Cago', 'Puto']}
         showRating
         size={20}
         startingValue={0}
         defaultRating={0}
+        onFinishRating={(rating) => setRating(rating)}
       />
-      <View style={{ flexDirection: "row" }}>
+      <View style={{ flexDirection: 'row' }}>
         <TextInput
           style={styles.input}
           placeholder="Here you can rate this job"
           textAlignVertical="top"
           multiple
+          onChangeText={setText}
           numberOfLines={4}
         />
       </View>
+      <Pressable style={[styles.button, styles.buttonClose]} onPress={handleSubmit}>
+        <Text style={styles.textStyle}>Enviar</Text>
+      </Pressable>
     </>
   );
 };
@@ -31,8 +62,24 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 20,
-    flexWrap: "wrap",
+    flexWrap: 'wrap',
     flex: 1,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
   },
 });
 

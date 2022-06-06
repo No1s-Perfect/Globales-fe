7816ec0a8050d85b131d03caedb0eb850d4c3f15
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-06 17:56:50 -0600

    Add files via upload

--- globales-fe/navigators/RootStack.js
+++ globales-fe/navigators/RootStack.js
@@ -1,47 +1,45 @@
 import React from 'react';
 
-import {Colors} from './../components/styles';
-const {primary, tertiary} = Colors;
-
+import { Colors } from './../components/styles';
+const { primary, tertiary } = Colors;
+import Toast from 'react-native-toast-message';
 //React navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 //screens
 import Login from './../screens/Login';
 import Signup from './../screens/Signup';
 import Welcome from './../screens/Welcome';
-
+import Tostada from '../components/Tostada';
 const Stack = createStackNavigator();
 
 const RootStack = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerStyled: {
-                        backgroundColor: 'transparent'
-                    },
-                    headerTintColor: tertiary,
-                    headerTransparent: true,
-                    headerTitle: '',
-                    headerLeftContainerStyle:{
-                        paddingLeft: 20
-                    }
-                }}
-                initialRouteName="Login"
-            >
-
-
-
-
-                <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name="Signup" component={Signup}/>
-                <Stack.Screen option={{ headerTintColor: primary  }}name="Welcome" component={Welcome}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-
-    )
-} 
+  return (
+    <>
+      <NavigationContainer>
+      <Tostada offSet={40}/>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyled: {
+              backgroundColor: 'transparent',
+            },
+            headerTintColor: tertiary,
+            headerTransparent: true,
+            headerTitle: '',
+            headerLeftContainerStyle: {
+              paddingLeft: 20,
+            },
+          }}
+          initialRouteName="Login"
+        >
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen option={{ headerTintColor: primary }} name="Welcome" component={Welcome} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
 
 export default RootStack;

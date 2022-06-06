8bf20c8357d030b46b312a3fa51e20e6476fe5f7
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-05 22:55:57 -0600

    Update Cards.js

--- globales-fe/screens/Cards.js
+++ globales-fe/screens/Cards.js
@@ -1,40 +1,34 @@
-import { useState,useEffect } from "react";
-import ModalReview from "../components/ModalReview";
-import { View, ScrollView } from "react-native";
-import { SearchBar } from "react-native-elements";
-import Tostada from "../components/Tostada";
-import CardList from "../components/CardList";
-
+import { useState, useEffect } from 'react';
+import ModalReview from '../components/ModalReview';
+import { View, ScrollView } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import Tostada from '../components/Tostada';
+import CardList from '../components/CardList';
+import { url } from '../components/Constants';
 //API client
 import axios from 'axios';
+import Spinner from '../components/Spinner';
 
 const Cards = () => {
   const [show, setShow] = useState(false);
-  const [search, setSearch] = useState("");
-  const [infoCards, setInfoCards] = useState([  ]);
-  const handleOffers = async() => {
-    
-    const url= 'https://dbcc-186-179-64-43.ngrok.io/offers';
- 
-  await axios
-    .get(url)
-    .then((response) => {
-        const result = response;
-        const {status, data} = result;
-        if(status!=200){
-          console.log("status 200")
-        }else{
-            console.log("all ok")
-            setInfoCards(data)
-        }
-    })
-    .catch((error)=> {
-      console.log(error);
-    });
-};
-useEffect(() => {
-  handleOffers();
-}, []);
+  const [search, setSearch] = useState('');
+  const [infoCards, setInfoCards] = useState([]);
+  const [fetching, setFetching] = useState(false);
+  const handleOffers = async () => {
+    try {
+      setFetching(true);
+      const res = await axios.get(url.offers);
+      console.log(res.data);
+      setInfoCards(res.data);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setFetching(false);
+    }
+  };
+  useEffect(() => {
+    handleOffers();
+  }, []);
   return (
     <View>
       <Tostada />
@@ -47,14 +41,19 @@ useEffect(() => {
       />
       <ScrollView>
         <View style={{ marginBottom: 100 }}>
-          <CardList
-            infoCards={
-              search.length > 0
-              ? infoCards.filter((info) => info.title == search)
-              : infoCards
-            }
-            setShow={setShow}
-          />
+          {!fetching && (
+            <CardList
+              infoCards={search.length > 0 ? infoCards.filter((info) => info.title == search) : infoCards}
+              setShow={setShow}
+            />
+          )}
+          {fetching && (
+            <View
+              style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+            >
+              <Spinner />
+            </View>
+          )}
         </View>
       </ScrollView>
       <ModalReview show={show} setShow={setShow} />
@@ -63,3 +62,4 @@ useEffect(() => {
 };
 
 export default Cards;
+

2e1edd3ed0de40c44d67b9b4f54aa91433495927
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-06 18:28:06 -0600

    Add files via upload

---
+++ globales-fe/Constants.js
@@ -0,0 +1,7 @@
+const domain = "https://fathomless-dawn-57488.herokuapp.com/"
+export const url = {
+    feedback:`${domain}feedback`,
+    offers:`${domain}offers`,
+    login:`${domain}login`,
+    addUser:`${domain}addUser`,
+}

aa00db236838b8c160a051806923cca3d5efa2e4
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-19 17:53:33 -0600

    Update EditOffer.js

--- globales-fe/components/EditOffer.js
+++ globales-fe/components/EditOffer.js
@@ -1,37 +1,46 @@
 import { AirbnbRating } from 'react-native-ratings';
 import { TextInput, StyleSheet, Pressable, Text, View } from 'react-native';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import Toast from "react-native-toast-message";
 import UserContext from './context/UserContext';
 import { url } from './Constants';
-const EditOffer = ({setShow,setFetchReview, idOferta}) => {
+const EditOffer = ({setShow,setFetchReview, idOferta, oferta, cb}) => {
   const [text, setText] = useState('');
   const [descripcion, setDescripcion] = useState('');
   const [precioServicio, setPrecioServicio] = useState(0.0);
   const [ubicacion, setUbicacion] = useState('');
   const [numTelefono, setNumTelefono] = useState(0);
 
+  useEffect(()=>{
+    setText(oferta.tituloOferta)
+    setUbicacion(oferta.ubicacion)
+    setDescripcion(oferta.descripcionOferta)
+    setNumTelefono(oferta.numTelefono)
+    setPrecioServicio(oferta.precioServicio)
+  },[])
   const handleSubmit = async() => {
 
     try{
-      console.log(idOferta,'asdasdasdasd')
       setFetchReview(true)
+      console.log(precioServicio,numTelefono,idOferta)
+      const precio = parseInt(precioServicio)
+      const num = parseInt(numTelefono)
       await axios.post(`${url.offers}/${idOferta}`,{
         titulo : text, 
         descripcion: descripcion,
-        precioServicio: precioServicio,
+        precioServicio: precio,
         ubicacion:ubicacion,
-        numTelefono: numTelefono
+        numTelefono: num
       })
-      
+      cb(idOferta, text, ubicacion, descripcion, num, precio )
       Toast.show({
         type: "success",
         text2: "Everything went smoothly 👋",
       });
       
     }catch(e){
-      console.log(e)
+    
     }
     finally{
       setFetchReview(false)
@@ -45,6 +54,7 @@ const EditOffer = ({setShow,setFetchReview, idOferta}) => {
     <TextInput
       style={styles.input}
       placeholder="Titulo"
+      value={text}
       textAlignVertical="top"
       multiple
       onChangeText={setText}
@@ -54,6 +64,7 @@ const EditOffer = ({setShow,setFetchReview, idOferta}) => {
   <View style={{ flexDirection: 'row' }}>
     <TextInput
       style={styles.input}
+      value={ubicacion}
       placeholder="Ubicación"
       textAlignVertical="top"
       multiple
@@ -65,6 +76,7 @@ const EditOffer = ({setShow,setFetchReview, idOferta}) => {
     <TextInput
       style={styles.input}
       placeholder="Descripción"
+      value={descripcion}
       textAlignVertical="top"
       multiple
       onChangeText={setDescripcion}
@@ -73,18 +85,23 @@ const EditOffer = ({setShow,setFetchReview, idOferta}) => {
   </View><View style={{ flexDirection: 'row' }}>
     <TextInput
       style={styles.input}
+      value={numTelefono.toString()}
       placeholder="Número de contacto"
       textAlignVertical="top"
       multiple
       onChangeText={setNumTelefono}
+      keyboardType='phone-pad'
+      maxLength={9}
       numberOfLines={4}
     />
   </View><View style={{ flexDirection: 'row' }}>
     <TextInput
       style={styles.input}
       placeholder="Precio por hora"
+      value={precioServicio.toString()}
       textAlignVertical="center"
       multiple
+      keyboardType="numeric"
       onChangeText={setPrecioServicio}
       numberOfLines={4}
     />

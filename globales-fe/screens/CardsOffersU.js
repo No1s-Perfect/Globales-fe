7250487a36cb9dc1ef8e47fa3395b3aa2bfafb51
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2022-06-19 17:51:39 -0600

    Update CardsOffersU.js

--- globales-fe/screens/CardsOffersU.js
+++ globales-fe/screens/CardsOffersU.js
@@ -1,6 +1,6 @@
-import { useState, useEffect, useContext,useCallback } from 'react';
+import { useState, useEffect, useContext, useCallback } from 'react';
 import ModalReview from '../components/ModalEditOffer';
-import { View, ScrollView , RefreshControl} from 'react-native';
+import { View, ScrollView, RefreshControl } from 'react-native';
 import Tostada from '../components/Tostada';
 import CardListOffersU from '../components/CardListOffersU';
 import { url } from '../components/Constants';
@@ -8,33 +8,47 @@ import { url } from '../components/Constants';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 
-
 import UserContext from '../components/context/UserContext';
-const CardsOffersU = ({navigation}) => {
+
+const CardsOffersU = ({ navigation }) => {
   const { user, setUser } = useContext(UserContext);
 
   const [show, setShow] = useState(false);
   const [infoCards, setInfoCards] = useState([]);
   const [fetching, setFetching] = useState(false);
-  const [idOferta,setIdOferta] = useState(-1)
+  const [idOferta, setIdOferta] = useState(-1);
   const handleOffers = async () => {
     try {
       setFetching(true);
-      const res = await axios.get( `${url.userOffers}/${user.id}`);
-      console.log(res.data);
+      const res = await axios.get(`${url.userOffers}/${user.id}`);
+
       setInfoCards(res.data);
     } catch (e) {
-      console.log(e);
     } finally {
       setFetching(false);
     }
   };
   const onRefresh = useCallback(() => {
-    handleOffers()
+    handleOffers();
   }, []);
 
+  const cb = (idOferta, titulo, lugar, descripcion, telefono, precio) =>
+    setInfoCards(
+      infoCards.map((card) =>
+        idOferta === card.idOferta
+          ? {
+              ...card,
+              tituloOferta: titulo,
+              ubicacion: lugar,
+              descripcionOferta: descripcion,
+              numTelefono: telefono,
+              precioServicio: precio,
+            }
+          : card,
+      ),
+    );
   useEffect(() => {
-    console.log('cards');
+    console.log('cardsoffers');
     handleOffers();
   }, []);
   return (
@@ -42,13 +56,7 @@ const CardsOffersU = ({navigation}) => {
       <Tostada />
       <ScrollView refreshControl={<RefreshControl refreshing={fetching} onRefresh={onRefresh} />}>
         <View style={{ marginBottom: 100 }}>
-          {!fetching && (
-            <CardListOffersU
-              infoCards={infoCards}
-              setShow={setShow}
-              setIdOferta={setIdOferta}
-            />
-          )}
+          {!fetching && <CardListOffersU infoCards={infoCards} setShow={setShow} setIdOferta={setIdOferta} editing />}
           {fetching && (
             <View
               style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
@@ -58,7 +66,15 @@ const CardsOffersU = ({navigation}) => {
           )}
         </View>
       </ScrollView>
-      <ModalReview show={show} idOferta={idOferta}setShow={setShow} />
+      {show && (
+        <ModalReview
+          show={show}
+          cb={cb}
+          oferta={infoCards.find((ofer) => idOferta === ofer.idOferta)}
+          idOferta={idOferta}
+          setShow={setShow}
+        />
+      )}
     </View>
   );
 };
